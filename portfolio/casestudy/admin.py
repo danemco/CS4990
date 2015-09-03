edb2355776ae342efbe1247429c63db9a98943e6
Dan Purcell <dan@velocitywebworks.com>
2015-09-02 18:42:11 -0600

    Added admin

--- portfolio/casestudy/admin.py
+++ portfolio/casestudy/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CaseStudy, Item
+
+class CaseStudyAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CaseStudy, CaseStudyAdmin)
+admin.site.register(Item)
+
+
