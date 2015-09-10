4aedff49445ec8723651dad5b0282576a21e6f81
Dan Purcell <dan@velocitywebworks.com>
2015-09-09 18:38:11 -0600

    Using bootstrap as a base template

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,5 +1,6 @@
 """portfolio URL Configuration
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
