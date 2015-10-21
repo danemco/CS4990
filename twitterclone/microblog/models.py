6b3764f7b24bb36315d041b2672dcf1b44f5cbf9
Dan Purcell <dan@velocitywebworks.com>
2015-10-20 22:09:51 -0600

    Fixing newb errors

--- twitterclone/microblog/models.py
+++ twitterclone/microblog/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User)
     bio  = models.TextField('Short Bio', blank=True, null=True)
     profile_picture = models.ImageField(upload_to="pics/%Y/%m/%d", blank=True, null=True)
-    following = models.ManyToMany('this', blank=True, null=True)
+    following = models.ManyToManyField('self')
 
     def __unicode__(self):
         return self.user.get_full_name() or self.user.username
